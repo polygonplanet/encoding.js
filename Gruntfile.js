bc7dfcad1124c689d0993e6cfccc88a5e850364a
polygonplanet <polygon.planet.aqua@gmail.com>
2016-11-03 07:55:53 +0900

    Add minify task

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = function(grunt) {
+  var pkg = grunt.file.readJSON('package.json');
+
+  grunt.util.linefeed = '\n';
+
+  grunt.initConfig({
+    pkg: pkg,
+    uglify: {
+      build: {
+        options: {
+          ASCIIOnly: true,
+          maxLineLen: 8000,
+          banner: [
+            '/*!',
+            ' * <%= pkg.name %> v<%= pkg.version %> - <%= pkg.description %>',
+            ' * Copyright (c) 2013-2016 <%= pkg.author %>',
+            ' * <%= pkg.homepage %>',
+            ' * @license <%= pkg.license %>',
+            ' */',
+            ''
+          ].join('\n')
+        },
+        files: {
+          './encoding.min.js': './encoding.js'
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.registerTask('default', ['uglify']);
+};
